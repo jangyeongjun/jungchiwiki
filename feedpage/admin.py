1fde62b28f54577c9d6bd48162d5fabd333098e0
yeongjun <dudwns051230@gmail.com>
2020-07-30 18:55:59 +0900

    merge complete

--- feedpage/admin.py
+++ feedpage/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import *
-from .models import Politician
 from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
-admin.site.register(Politician)
+
 admin.site.register(NormalFeed)
 admin.site.register(SmallFeed)
 admin.site.register(Comment)
